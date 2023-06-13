fabf24e8e78d46bde18878a10cc26bcecf8d66cc
Max Peerutin <peerutinm@gmail.com>
2023-06-13 10:53:54 +0200

    Initial Commit

---
+++ SourceControlXcode14/SourceControlXcode14App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode14App.swift
+//  SourceControlXcode14
+//
+//  Created by Max Peerutin on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
