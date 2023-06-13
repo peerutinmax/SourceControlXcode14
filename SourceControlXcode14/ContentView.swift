6cbf8f52cad5227150da36c7fef75acb9c56e8fd
Max Peerutin <peerutinm@gmail.com>
2023-06-13 11:11:09 +0200

    added a font

--- SourceControlXcode14/ContentView.swift
+++ SourceControlXcode14/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
                 .font(.title)
+            Text("Hope you are well!")
         }
         .padding()
     }
